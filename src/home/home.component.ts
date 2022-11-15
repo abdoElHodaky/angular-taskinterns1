4d7d9223116a9eb64b0e680b772ab3f1b6452301
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2022-11-15 11:09:43 +0200

    2 files modified

--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   title = 'CodeSandbox';
+  slides = [
+    { img: 'https://via.placeholder.com/600.png/09f/fff' },
+    { img: 'https://via.placeholder.com/600.png/021/fff' },
+    { img: 'https://via.placeholder.com/600.png/321/fff' },
+    { img: 'https://via.placeholder.com/600.png/422/fff' },
+    { img: 'https://via.placeholder.com/600.png/654/fff' },
+  ];
+  slideConfig = {
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    infinite: true,
+  };
+  addSlide() {
+    this.slides.push({ img: 'http://placehold.com/600.png/' });
+  }
+  removeSlide() {
+    this.slides.length = this.slides.length - 1;
+  }
+  slickInit(e: any) {
+    console.log('slick initialized');
+  }
+  breakpoint(e: any) {
+    console.log('breakpoint');
+  }
+  afterChange(e: any) {
+    console.log('afterChange');
+  }
+  beforeChange(e: any) {
+    console.log('beforeChange');
+  }
 }
