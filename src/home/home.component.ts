d4398a23b379afbf83453382f8f2df46d05e24a9
abdoElHodaky <47593167+abdoElHodaky@users.noreply.github.com>
2022-11-21 15:25:10 +0200

    2 files modified

--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -1,46 +1,46 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "<app-home></app-home>",
-  templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.css"]
+  selector: '<app-home></app-home>',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  title = "CodeSandbox";
+  title = 'CodeSandbox';
   slides = [
-    { img: './src/assetss/hugo-productive-work.png', p: 'hello' },
-    { img: './src/assetss/hugo-productive-work.png', p: 'abdo' },
-    { img: './src/assetss/hugo-productive-work.png', p: 'moh' },
-    { img: './src/assets/hugo-productive-work.png', p: 'arh' },
-    { img: './src/assets/hugo-productive-work.png', p: 'bye' },
+    { img: '../src/assetss/hugo-productive-work.png', p: 'hello' },
+    { img: '../src/assetss/hugo-productive-work.png', p: 'abdo' },
+    { img: '../src/assetss/hugo-productive-work.png', p: 'moh' },
+    { img: '../src/assets/hugo-productive-work.png', p: 'arh' },
+    { img: '../src/assets/hugo-productive-work.png', p: 'bye' },
   ];
-    slideConfig = {
-      adaptiveHeight:true,
-      swipe:true,
-      mobileFirst:true,
-      arrows: false,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      dots: true,
-      infinite: true,
-      rtl: true,
-    };
-    addSlide() {
-      this.slides.push({ img: '../assets/hugo-productive-work.png', p: 'hello' });
-    }
-    removeSlide() {
-      this.slides.length = this.slides.length - 1;
-    }
-    slickInit(e: any) {
-      console.log('slick initialized');
-    }
-    breakpoint(e: any) {
-      console.log('breakpoint');
-    }
-    afterChange(e: any) {
-      console.log('afterChange');
-    }
-    beforeChange(e: any) {
-      console.log('beforeChange');
-    }
+  slideConfig = {
+    adaptiveHeight: true,
+    swipe: true,
+    mobileFirst: true,
+    arrows: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    infinite: true,
+    rtl: true,
+  };
+  addSlide() {
+    this.slides.push({ img: '../assets/hugo-productive-work.png', p: 'hello' });
+  }
+  removeSlide() {
+    this.slides.length = this.slides.length - 1;
+  }
+  slickInit(e: any) {
+    console.log('slick initialized');
+  }
+  breakpoint(e: any) {
+    console.log('breakpoint');
+  }
+  afterChange(e: any) {
+    console.log('afterChange');
+  }
+  beforeChange(e: any) {
+    console.log('beforeChange');
+  }
 }
